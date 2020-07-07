c3ab3dede4b4dcf3ff26b8bab0cb19ccafdffba8
LarocheJ <jimmy.lar91@gmail.com>
2020-07-07 13:56:15 -0600

    remove page1 and page 2

--- pages/page2.php
+++
@@ -1,9 +0,0 @@
-<?php include('../includes/head.php'); ?>
-
-<div class="container">
-    <h1>Page 2</h1>
-    <a href="#" class="primary-btn" id="goBack">< Back</a>
-    <a class="primary-btn" href="/pages/page3.php">Save and continue ></a>
-</div>
-
-<?php include('../includes/footer.php'); ?>
--- pages/page3.php
+++
@@ -1,8 +0,0 @@
-<?php include('../includes/head.php'); ?>
-
-<div class="container">
-    <h1>Page 3</h1>
-    <a href="javascript:;" class="primary-btn" id="goBack">< Back</a>
-</div>
-
-<?php include('../includes/footer.php'); ?>
