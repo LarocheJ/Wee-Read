7bc7345a377d48b104a2efe841060fabdd103645
LarocheJ <jimmy.lar91@gmail.com>
2020-08-12 12:55:24 -0600

    fix for-schools link

--- includes/footer.php
+++ includes/footer.php
@@ -20,7 +20,7 @@
                 <ul>
                     <li><a href="<?php print $home ?>/pages/wee-read">wee read</a></li>
                     <li><a href="<?php print $home ?>/pages/pee-wee-read">pee wee read</a></li>
-                    <li><a href="<?php print $home ?>/pages/for-schools.php">for schools</a></li>
+                    <li><a href="<?php print $home ?>/pages/for-schools">for schools</a></li>
                     <li><a href="<?php print $home ?>/pages/contact.php">contact</a></li>
                     <?php if(isset($_SESSION['email'])) { ?>
                         <li><a href="<?php print $home ?>/pages/profile.php">profile</a></li>
