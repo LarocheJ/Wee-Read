de0212739814085f8614219f96e6d503b8324904
Dan Stephenson <dan@danstephenson.ca>
2020-08-25 10:36:02 -0600

    Changing URL Prefix & Google Analytics
    Removing the /~alex/ root from domains, and adding Google Analytics code.

--- includes/functions.php
+++ includes/functions.php
@@ -3,7 +3,7 @@ function checkIfLoggedIn() {
     if($_SERVER['HTTP_HOST'] == "localhost") {
         $home = "//" . $_SERVER['HTTP_HOST']. "/sites/wee-read";
     } else {
-        $home = "//" . $_SERVER['HTTP_HOST'].'/~alex';
+        $home = "//" . $_SERVER['HTTP_HOST'].'';
     }
 
     if(!isset($_SESSION['email'])) {
